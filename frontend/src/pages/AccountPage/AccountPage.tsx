609047a3f41848808b50d946098caf9e31faa884
Raphael <raphael22522@outlook.com>
2026-03-30 20:34:54 -0300

    Bank Interface Completed

--- frontend/src/pages/AccountPage/AccountPage.tsx
+++ frontend/src/pages/AccountPage/AccountPage.tsx
@@ -1,4 +1,5 @@
 import { MainLayout } from '../../layouts/MainLayout/MainLayout';
+import accountIcon from '../../assets/account.png';
 import './AccountPage.css';
 
 export function AccountPage() {
@@ -6,7 +7,13 @@ export function AccountPage() {
         <MainLayout>
             <div className='account-layout'>
             <div className='side-bar'>
-                <h2 className='side-bar-title'>Accounts</h2>
+                <ul>
+                    <li>
+                        <img src={accountIcon} alt="Account" className='account-icon'/>
+                        <p>Account</p>    
+                    </li>
+                    
+                </ul>
             </div>
             <div className='account-info'>
                 <p className='account-title'>Checking</p>
@@ -15,6 +22,18 @@ export function AccountPage() {
                 <p className='account-value'>$1000.00</p>
                 <p className='account-title'>Currency:</p>
                 <p className='account-value'>USD</p>
+                <div className='transaction-options'>
+                    <form action="">
+                        <input type="number" />
+                        <button type='submit'>Deposit</button>
+                    </form>
+                </div>
+                <div className='transaction-options'>
+                    <form action="">
+                        <input type="number" />
+                        <button type='submit'>Withdraw</button>
+                    </form>
+                </div>
             </div>
             <div className='account-actions'>
                 <h1 className='transaction-history-title'>Transaction History</h1>
